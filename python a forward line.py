eaf2e49a499c1d50f2e8f2850947772045bdb819
Apurva-Rachala <52021940+Apurva-Rachala@users.noreply.github.com>
2019-06-29 14:15:44 +0530

    Add files via upload

---
+++ python a forward line.py
@@ -0,0 +1,8 @@
+#step1:Make all the "turtle" package to be imported
+import turtle as tt
+# Turtle method creates and returns the new object
+a1=tt.Turtle()
+#forward method moves 100 pixels
+tt.forward(100)
+#we are done
+tt.done()
